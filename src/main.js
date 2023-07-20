c4779bf2ab516880bf8a7fb42bde424aba2ecde9
tanya-kostecki <tanya.pavlova1294@gmail.com>
2023-07-20 15:36:17 +0200

    задание 2

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('.container')
+//const container = document.querySelector('.container')
 
 const startButton = document.querySelector('.start-button')
 const containerLevels = document.querySelectorAll('.container-level')
@@ -7,6 +7,7 @@ let selectedLevel
 
 startButton.addEventListener('click', () => {
     let isChecked = false
+    let numberOfPairs
     for (const containerLevel of containerLevels) {
         if (containerLevel.checked === true) {
             isChecked = true
@@ -18,16 +19,103 @@ startButton.addEventListener('click', () => {
         return
     }
 
-    GamePageRender(selectedLevel)
+    if (selectedLevel === '1') {
+        numberOfPairs = 3
+    }
+    if (selectedLevel === '2') {
+        numberOfPairs = 6
+    }
+    if (selectedLevel === '3') {
+        numberOfPairs = 9
+    }
+
+    GamePageRender(numberOfPairs)
 })
 
-const GamePageRender = (level) => {
-    container.innerHTML = ``
-    let gamePageHtml = `
-       <div class="container-content">
-            <h1 class="container-title">Уровень сложности:<label class="radio-label">${level}</label></h1>
-       </div> `
+const cardSuits = ['Diamonds', 'Clubs', 'Spades', 'Hearts']
+const cardRanks = ['6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
 
-    container.innerHTML = gamePageHtml
+const body = document.querySelector('body')
+const GamePageRender = (numberOfPairs) => {
+    body.innerHTML = `
+        <div class="game-page-container">
+            <div class="timer">
+                <div class="time">
+                    <span class="timer-number">00.</span>
+                    <span>min</span>
+                </div>
+                <div class="time">
+                    <span class="timer-number">00</span>
+                    <span>sec</span>
+                </div>
+            </div>
+            <div class="button">
+                <button class="restart-button">Начать заново</button>
+            </div>
+        </div>
+        <div class="game-container">
+            <div class="game-space"></div>
+        </div>`
+
+    let setOfCards = []
+
+    const gameSpace = document.querySelector('.game-space')
+    for (let i = 0; i < numberOfPairs; i++) {
+        const randomCardSuits = Math.floor(Math.random() * cardSuits.length)
+        const randomCardRanks = Math.floor(Math.random() * cardRanks.length)
+
+        let cardElement =
+            '../img/images/' +
+            cardRanks[randomCardRanks] +
+            cardSuits[randomCardSuits] +
+            '.jpg'
+
+        setOfCards.push(cardElement, cardElement)
+        console.log(setOfCards)
+    }
+
+    const shuffledSetOfCards = setOfCards.sort(() => Math.random() - 0.5)
+    for (let j = 0; j < shuffledSetOfCards.length; j++) {
+        const cardElem = document.createElement('img')
+        cardElem.src = setOfCards[j]
+        gameSpace.appendChild(cardElem)
+        cardElem.classList.add('game-card')
+    }
+
+    const gameCards = document.querySelectorAll('.game-card')
+    const hideGameCards = () => {
+        for (const gameCard of gameCards) {
+            gameCard.src = '../img/background/background.jpg'
+        }
+    }
+    setTimeout(hideGameCards, 5000)
+
+    //Клик на рубашку карточки
+    const compareGameCards = (gameCards, setOfCards) => {
+        let clickedCards = []
+        let matchCounter = 0
+        for (let i = 0; i < gameCards.length; i++) {
+            const gameCard = gameCards[i]
+            gameCard.addEventListener('click', (event) => {
+                event.target.src = setOfCards[i]
+                if (!clickedCards[0]) {
+                    clickedCards[0] = setOfCards[i]
+                } else {
+                    clickedCards[1] = setOfCards[i]
+
+                    if (clickedCards[0] === clickedCards[1]) {
+                        matchCounter++
+
+                        if (matchCounter === setOfCards.length / 2) {
+                            alert('You win')
+                        }
+                    } else {
+                        alert('You lose')
+                    }
+                    clickedCards = []
+                }
+            })
+        }
+    }
+    compareGameCards(gameCards, setOfCards)
 }
-console.log(containerLevels)
